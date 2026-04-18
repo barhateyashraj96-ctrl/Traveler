484ece714af80c890114668958e2be15fb0ea90e
Yashraj <barhateyashraj96@gmail.com>
2026-04-18 15:11:33 +0530

    nav responsive

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,57 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <nav >
-          <ul>
-            <li className='tabs'>
-              <b className='d'>Sai Shubham Tours And Travels</b>
-              <Link to="/"><h2 style={{ paddingTop: '15px' }}>Home</h2></Link>
-              <Link to="about"><h2 style={{ paddingTop: '15px' }}>About</h2></Link>
-              <Link to="/gallery"><h2 style={{ paddingTop: '15px' }}>Gallery</h2></Link>
-              <Link to="/services"><h2 style={{ paddingTop: '15px' }}>Services</h2></Link>
-              <Link to="/contact"><h2 style={{ paddingTop: '15px' }}>Contact</h2></Link>
-              <Link to="/contact"><button className='c'>Book Now</button></Link>
-            </li>
-          </ul>
-        </nav>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
 
+          {/* Brand */}
+          <Link className="navbar-brand fw-bold" to="/">
+            Sai Shubham Tours And Travels
+          </Link>
+
+          {/* Toggle Button (Mobile) */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Menu */}
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto align-items-lg-center">
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">About</Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/gallery">Gallery</Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/services">Services</Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </li>
+
+              {/* Button */}
+              <li className="nav-item ms-lg-3 mt-2 mt-lg-0">
+                <Link to="/contact" className="btn btn-primary">
+                  Book Now
+                </Link>
+              </li>
+
+            </ul>
+          </div>
+        </nav>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
